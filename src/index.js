97c174313a31f6c87024c6d3f442eb821bc46221
connor <mcwvproductions@gmail.com>
2019-05-08 23:00:25 -0700

    added 404 support

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Store from './pages/store/Store';
 import Tester from './pages/Tester'
+import Notfound from './pages/Notfound';
 import * as serviceWorker from './serviceWorker';
+
 
 const routing = (
     <Router>
         <div>
             <Route exact path="/" component={Store} />
             <Route path="/test" component={Tester} />
+            
+            {/* DONT TOUCH THIS ONE, IT MUST STAY AT THE BOTTOM OF THE LIST */}
+            <Route component={Notfound} />
         </div>
     </Router>
 )
