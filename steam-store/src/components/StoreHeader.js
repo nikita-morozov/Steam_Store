86df2d510c6b2831ec97602cf8f110d2412aef29
Nikita <nikita1020@gmail.com>
2019-05-08 18:26:34 -0700

    Creates basic layout

---
+++ steam-store/src/components/StoreHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './StoreHeader.css';
+
+function StoreHeader() {
+  return (
+    <div className="StoreHeader">
+    
+    </div>
+  );
+}
+
+export default StoreHeader;
---
+++ steam-store/src/pages/store/Store.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Store.css';
+import '../../components/StoreHeader.js'
+
+function Store() {
+  return (
+    
+    <div className="Store">
+        <p>
+          Store Page Main
+        </p>
+    </div>
+  );
+}
+
+export default Store;
