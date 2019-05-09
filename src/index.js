0313e69ecf6d343df40ab0c4a64152e1126441c1
connor <mcwvproductions@gmail.com>
2019-05-08 22:54:01 -0700

    Baseline; working on it

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Store from './pages/store/Store';
+import Tester from './pages/Tester'
 import * as serviceWorker from './serviceWorker';
+
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={Store} />
+            <Route path="/test" component={Tester} />
+        </div>
+    </Router>
+)
 
 ReactDOM.render(<Store />, document.getElementById('root'));
 serviceWorker.unregister();
