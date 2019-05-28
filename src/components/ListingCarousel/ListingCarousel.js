e791c5d1fb8dee25b2f1193ba0b26bbb1c57f6db
Nikita Morozov <nikita1020@gmail.com>
2019-05-27 22:05:34 -0700

    Listing page part 5

--- src/components/ListingCarousel/ListingCarousel.js
+++ src/components/ListingCarousel/ListingCarousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
+import "./ListingCarousel.css";
 import { bdb } from '../../db.js';
 
 
@@ -10,28 +11,28 @@ class ListingCarousel extends React.Component {
     render() {
         const images = [
             {
-                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c1"],
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["a1"],
                 thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c1"],
             },
             {
-                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c2"],
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["a2"],
                 thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c2"]
             },
             {
-                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c3"],
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["a3"],
                 thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c3"]
             },
             {
-                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c4"],
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["a4"],
                 thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c4"]
             },
             {
-                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c5"],
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["a5"],
                 thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c5"]
             }
         ]
         return (
-            <ImageGallery items={images} showPlayButton={false} />
+            <ImageGallery items={images} showPlayButton={false} showFullscreenButton={true} />
         );
     }
 
--- src/pages/Listing/Listing.js
+++ src/pages/Listing/Listing.js
@@ -51,6 +51,50 @@ class Listing extends React.Component {
                 <div id='image'>
                   <PromoImage src={this.state.promoloc} imgname={this.state.imgname} title={this.state.title} />
                 </div>
+                <div id="description">
+                  <p>The classic multiplayer roguelike, Risk of Rain, returns with an extra dimension and more challenging action. Play solo, or team up with up to three friends to fight your way through hordes of monsters, unlock new loot, and find a way to escape the planet.</p>
+                  <div class="user_reviews">
+                    <div class="user_reviews_summary_row">
+                      <div class="subtitleColumn">Recent Reviews:</div>
+                      <div class="summaryColumn">
+                        <span class="game_review_summaryPositive">Very Positive </span>
+                        <span class="responsive_hidden">
+                          (2,116)
+                            </span>
+                      </div>
+                    </div>
+                    <div class="user_reviews_summary_row">
+                      <div class="subtitleColumn">All Reviews:</div>
+                      <div class="summaryColumn">
+                        <span class="game_review_summaryPositive" itemprop="description">Very Positive </span>
+                        <span class="responsive_hidden">
+                          (15,675)
+                            </span>
+                        <meta itemprop="reviewCount" content="15675" />
+                        <meta itemprop="ratingValue" content="9" />
+                        <meta itemprop="bestRating" content="10" />
+                        <meta itemprop="worstRating" content="1" />
+                      </div>
+                    </div>
+                    <div class="release_date">
+                      <div class="subtitleColumn">Release Date:</div>
+                      <div class="date">Mar 28, 2019</div>
+                    </div>
+                    <div class="dev_row">
+                      <div class="subtitleColumn">Developer:</div>
+                      <div class="summaryColumn" id="developers_list">
+                        <a id="listingLink" href="https://store.steampowered.com/search/?developer=Hopoo%20Games&amp;snr=1_5_9__400">Hopoo Games</a>
+                      </div>
+                    </div>
+                    <div class="dev_row">
+                      <div class="subtitleColumn">Publisher:</div>
+                      <div class="summaryColumn">
+                        <a id="listingLink" href="https://store.steampowered.com/search/?publisher=Gearbox%20Publishing&amp;snr=1_5_9__422">Gearbox Publishing</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="tags"></div>
+                </div>
               </div>
               <div class='queue'>
               </div>
