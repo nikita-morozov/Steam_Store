e80482572bd44b7edcc68ba7ed97499b12a50d80
Nikita Morozov <nikita1020@gmail.com>
2019-05-27 20:02:31 -0700

    Listing update

--- src/components/ListingCarousel/ListingCarousel.js
+++ src/components/ListingCarousel/ListingCarousel.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
+import { bdb } from '../../db.js';
+
 
 
 class ListingCarousel extends React.Component {
@@ -8,16 +10,24 @@ class ListingCarousel extends React.Component {
     render() {
         const images = [
             {
-                original: 'http://lorempixel.com/1000/600/nature/1/',
-                thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c1"],
+                thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c1"],
             },
             {
-                original: 'http://lorempixel.com/1000/600/nature/2/',
-                thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c2"],
+                thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c2"]
             },
             {
-                original: 'http://lorempixel.com/1000/600/nature/3/',
-                thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c3"],
+                thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c3"]
+            },
+            {
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c4"],
+                thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c4"]
+            },
+            {
+                original: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c5"],
+                thumbnail: bdb[window.location.href.substring(window.location.href.lastIndexOf('/') + 1)]["c5"]
             }
         ]
         return (
