ec9f8ae3065a13a4086ebcc8ccc5a61cd2cc1709
connor <mcwvproductions@gmail.com>
2019-05-27 18:10:06 -0700

    added more games and screens to the db

--- src/db.js
+++ src/db.js
@@ -7,15 +7,12 @@ export const bdb = (
             "oldprice": "$29.99",
             "title": "Risk of Rain 2",
             "img": {ror2},
-            "imgname": "ror2"
-        },
-
-        "ror4": {
-            "price": "$459.99",
-            "oldprice": "$699.99",
-            "title": "Risk of Rain 4 YUHHHH",
-            "img": {ror2},
-            "imgname": "ror2"
+            "imgname": "ror2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/632360/ss_76082d1683ec86903a7f071181c89ccaf78b857d.600x338.jpg?t=1558455404",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/632360/ss_121003c290030ab310e1651a442609939141911e.600x338.jpg?t=1558455404",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/632360/ss_d93451e8534c512fcf15e1d8c7a02e2277807aa7.600x338.jpg?t=1558455404",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/632360/ss_ebf09ff83af59cbf2735862d99085f43cf3515cc.600x338.jpg?t=1558455404",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/632360/ss_a8cc7f38532d4d05eb8cc825c2c23475aab01679.600x338.jpg?t=1558455404"
         },
 
         "dota2": {
@@ -23,7 +20,182 @@ export const bdb = (
             "oldprice": "",
             "title": "DOTA 2",
             "img": "https://steamcdn-a.akamaihd.net/steam/apps/570/header.jpg?t=1557267263",
-            "imgname": "d2"
-        }
+            "imgname": "d2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/570/ss_86d675fdc73ba10462abb8f5ece7791c5047072c.600x338.jpg?t=1557267263",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/570/ss_ad8eee787704745ccdecdfde3a5cd2733704898d.600x338.jpg?t=1557267263",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/570/ss_7ab506679d42bfc0c0e40639887176494e0466d9.600x338.jpg?t=1557267263",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/570/ss_c9118375a2400278590f29a3537769c986ef6e39.600x338.jpg?t=1557267263",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/570/ss_27b6345f22243bd6b885cc64c5cda74e4bd9c3e8.600x338.jpg?t=1557267263"
+        },
+
+        "hl2": {
+            "price": "$9.99",
+            "oldprice": "",
+            "title": "Half Life 2",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/220/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/220/0000001864.600x338.jpg?t=1541802014",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/220/0000001865.600x338.jpg?t=1541802014",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/220/0000001866.600x338.jpg?t=1541802014",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/220/0000001867.600x338.jpg?t=1541802014",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/220/0000001868.600x338.jpg?t=1541802014"
+        },
+
+        "biy": {
+            "price": "$14.99",
+            "oldprice": "",
+            "title": "Baba Is You",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/736260/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/736260/ss_908c8a4fd0ffec9f29341eb50eb7f074c105342f.600x338.jpg?t=1552492820",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/736260/ss_9eadb1cdc09f574d49d32a722d7bda5813c96a64.600x338.jpg?t=1552492820",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/736260/ss_c535fc337bdb15bfe35a164cc13db7ec92fd1f44.600x338.jpg?t=1552492820",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/736260/ss_e421ba3fb93e902eb4af05564ebf3afcabaae951.600x338.jpg?t=1552492820",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/736260/ss_4e489dcc482e3f094cd8caf0a10317fdc71345af.600x338.jpg?t=1552492820"
+        },
+
+        "beat-saber": {
+            "price": "$29.99",
+            "oldprice": "",
+            "title": "Beat Saber",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/620980/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/620980/ss_1881ae4f153faf0d1ccecca60fbdac5b43ad57eb.600x338.jpg?t=1558472129",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/620980/ss_114dc9a9f27666b2d56801ba49a1db8fa202b6ee.600x338.jpg?t=1558472129",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/620980/ss_b65444cc4513f34bd41fa6b0fe96cf11d94fea8d.600x338.jpg?t=1558472129",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/620980/ss_542d092f42c779c866167bec05c1da488bcd91f8.600x338.jpg?t=1558472129",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/620980/ss_910fb7ad48bfdd918b0396b14f3dd45fc7f2e847.600x338.jpg?t=1558472129"
+        },
+
+        "superhypercube": {
+            "price": "$14.99",
+            "oldprice": "",
+            "title": "SUPERHYPERCUBE",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/554870/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/554870/ss_c782f585a33a7bd64a3bf81ad758bc1d570bf7be.600x338.jpg?t=1510072209",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/554870/ss_c249c834ef341ea164992cb0542d79aedf2903fd.600x338.jpg?t=1510072209",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/554870/ss_4da0c6252546b3ac4a243843e10f55f4afdf45eb.600x338.jpg?t=1510072209",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/554870/ss_759d0fe1dc075319ccaae6befa780ffb54be4176.600x338.jpg?t=1510072209",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/554870/ss_bc2c2267e0d0922cbfea75f3e2326dd4ae08012c.600x338.jpg?t=1510072209"
+        },
+
+        "ffxiv": {
+            "price": "$19.99",
+            "oldprice": "",
+            "title": "FINAL FANTASY XIV Online",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/39210/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/39210/ss_ca57d5322b116ffaab2de37958df59eba8c7807a.600x338.jpg?t=1549473655",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/39210/ss_f1bc7a959795c1fad6bfc602ccbd0e311075a454.600x338.jpg?t=1549473655",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/39210/ss_ddb71dc0c9a667de5c23e0b3823dad3590d5e9bb.600x338.jpg?t=1549473655",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/39210/ss_912ffecab72b0ec4132e100582a6592e559ac3aa.600x338.jpg?t=1549473655",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/39210/ss_ed8445bec9d85b79079f7ec46be38adc4068e3c5.600x338.jpg?t=1549473655"
+        },
+
+        "nier": {
+            "price": "$39.99",
+            "oldprice": "",
+            "title": "NieR:Automata",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/524220/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/524220/ss_d0314b4c134329a483b5e43af951f60274abc66b.600x338.jpg?t=1556006070",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/524220/ss_8b29f7e1ce9a8b9313dc3eb50dbe76a4cf94eef9.600x338.jpg?t=1556006070",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/524220/ss_2c265df38c3d2d393d74ee8e74d79bdafa16b143.600x338.jpg?t=1556006070",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/524220/ss_831e0e7c9d514393b711e9ed1d6796042521a80c.600x338.jpg?t=1556006070",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/524220/ss_a6d164452c1aa00a0d7b7ca31aa76d787853b39e.600x338.jpg?t=1556006070"
+        },
+
+        "tw3k": {
+            "price": "$59.99",
+            "oldprice": "",
+            "title": "Total War: THREE KINGDOMS",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/779340/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/779340/ss_b367c99f566ecfc3def17673e9015c80e7e3a8d3.600x338.jpg?t=1558687446",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/779340/ss_fe3108d8affd7a9f7e72959602e49cb5fa105910.600x338.jpg?t=1558687446",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/779340/ss_16346541350f29271399420bffda0da34c8d5ea1.600x338.jpg?t=1558687446",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/779340/ss_ced0e321ac9262d29dc0a4e4fe4b91d3144ed2e6.600x338.jpg?t=1558687446",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/779340/ss_15164c8bb033b1c570865ab264ccaaab8131f564.600x338.jpg?t=1558687446"
+        },
+
+        "rust": {
+            "price": "$34.99",
+            "oldprice": "",
+            "title": "Rust",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/252490/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/252490/ss_c21e99d11d22a035744cfb974ee03647c1e628c5.600x338.jpg?t=1549762598",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/252490/ss_f78385eb47e11f046f696e09d5bfa4d329017b1e.600x338.jpg?t=1549762598",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/252490/ss_58c5b4107fbd90f5a56b3adc97a6b61384ce4f12.600x338.jpg?t=1549762598",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/252490/ss_1d6dd798ec9ebe3b6da69483bfcdd19e7300971c.600x338.jpg?t=1549762598",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/252490/ss_4f2d0e89f55bcf236bb64c72736104c44aa50275.600x338.jpg?t=1549762598"
+        },
+
+        "csgo": {
+            "price": "$14.99",
+            "oldprice": "",
+            "title": "Counter Strike: Global Offensive",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/730/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/730/ss_34090867f1a02b6c17652ba9043e3f622ed985a9.600x338.jpg?t=1554409309",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/730/ss_1d30c9a215fd621e2fd74f40d93b71587bf6409c.600x338.jpg?t=1554409309",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/730/ss_baa02e979cd3852e3c4182afcd603ab64e3502f9.600x338.jpg?t=1554409309",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/730/ss_ffe584c163a2b16e9c1b733b1c8e2ba669fb1204.600x338.jpg?t=1554409309",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/730/ss_d87c102d028d545c877363166c9d8377014f0c23.600x338.jpg?t=1554409309"
+        },
+
+        "r6s": {
+            "price": "$19.99",
+            "oldprice": "",
+            "title": "Tom Clancy's Rainbow Six® Siege",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/359550/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/359550/ss_891ce71ba6b2640270589687d58ef3be4b8fe044.600x338.jpg?t=1558096774",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/359550/ss_9350bedcb7f18819ac5e25e2cc436a10afcc2a38.600x338.jpg?t=1558096774",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/359550/ss_07da51726f2eac0e97a73175618ef60389eae3e2.600x338.jpg?t=1558096774",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/359550/ss_d9e0239ee7fd1ebd41ad968a06c960d804614b69.600x338.jpg?t=1558096774",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/359550/ss_f8b0572f49f5a25ddb2200d3fb5277ebabd482eb.600x338.jpg?t=1558096774"
+        },
+
+
+        "superhot": {
+            "price": "$19.99",
+            "oldprice": "",
+            "title": "SUPERHOT",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/322500/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/322500/ss_57d97f703754f546c1ca2f6b06de1db2245be4f1.600x338.jpg?t=1550065067",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/322500/ss_2bd988fa8395542a3f2ae125bd0dcacfab780d0c.600x338.jpg?t=1550065067",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/322500/ss_6230c8eeaf92f0a6e78dfdcd7b9a67a42fb9d9cf.600x338.jpg?t=1550065067",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/322500/ss_4a0fc6962982beffe22ece449a58bc2f84aa08c3.600x338.jpg?t=1550065067",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/322500/ss_2ee08f988e5c5499d50030f9e911c5494c45b1e7.600x338.jpg?t=1550065067"
+        },
+
+        "ikaruga": {
+            "price": "$9.99",
+            "oldprice": "",
+            "title": "Ikaruga",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/253750/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/253750/ss_5bf084bd00d557db5056e922db8be3098fdd265a.600x338.jpg?t=1447358720",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/253750/ss_d5c2b638afd8eefeba0a58a80e083589f4b5c59f.600x338.jpg?t=1447358720",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/253750/ss_e7c98893ad0a17250ad50075fba6efa12fcc5f7a.600x338.jpg?t=1447358720",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/253750/ss_8e8d14bebb20770bb1d2190e6caa43dfceada351.600x338.jpg?t=1447358720",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/253750/ss_f8bc69bdd5502bef836aff8c4b211c6905172457.600x338.jpg?t=1447358720"
+        },
+
+        "downwell": {
+            "price": "$2.99",
+            "oldprice": "",
+            "title": "Downwell",
+            "img": "https://steamcdn-a.akamaihd.net/steam/apps/360740/header.jpg?t=1541802014",
+            "imgname": "hl2",
+            "c1": "https://steamcdn-a.akamaihd.net/steam/apps/360740/ss_96fa27e853e9513de825df4264e356066a3c5145.600x338.jpg?t=1498680256",
+            "c2": "https://steamcdn-a.akamaihd.net/steam/apps/360740/ss_eaab64c0e734e568912fa3ff435e7bd09a28f70b.600x338.jpg?t=1498680256",
+            "c3": "https://steamcdn-a.akamaihd.net/steam/apps/360740/ss_c4f5edef3fb078d2c5483f8394cc5d500eb2e96c.600x338.jpg?t=1498680256",
+            "c4": "https://steamcdn-a.akamaihd.net/steam/apps/360740/ss_9e95c27552152d430797fa2a4c873e3ed8cab2ea.600x338.jpg?t=1498680256",
+            "c5": "https://steamcdn-a.akamaihd.net/steam/apps/360740/ss_ec8b0acf4fadae7315cfba1a85b0f04832d16576.600x338.jpg?t=1498680256"
+        },
     }
 )
